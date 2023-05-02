c0ff8059083b844ec128f108730354ab0611af71
ignais <ilapaz@iguanait.com>
2023-05-02 15:50:19 +0400

    update

---
+++ class_4.py
@@ -0,0 +1,33 @@
+'''
+Getter and Setter
+'''
+
+class PersonNonCorrect:
+    '''
+        This way isn't popular in Python community.
+    '''
+    
+    def __init__(self, name):
+        self.set_name(name)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        self._name = value
+        
+class PersonCorrect:
+    '''
+    This is the most popular way.
+    '''
+    
+    def __init__(self, name):
+        self.name = name
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name.upper()
